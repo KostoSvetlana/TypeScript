d1e4e321e6cd612f171ea4f08eeeb0f52e228ddc
Svetlana <Kostosv585@yandex.ru>
2022-07-04 20:35:09 +0300

    main

---
+++ src/interface.ts
@@ -0,0 +1,11 @@
+export interface ISearchFormData { 
+  'check-in-date': Date,
+  'check-out-date': Date,
+  'city': string,
+  'coordinates': [number, number],
+  'max-price': number,
+  }
+  
+export interface IPlaces { 
+    place?: ''
+  }
